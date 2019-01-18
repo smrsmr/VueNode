0a6b656bed70953036da73b475aa859b96c79cc5
smr <smr@qq.com>
2019-01-18 18:11:48 +0800

    js提取

--- vueAdmin/src/views/theRichText/h5Editor.vue
+++ vueAdmin/src/views/theRichText/h5Editor.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-    <vue-html5-editor :content="content" :height="500"></vue-html5-editor>
+    <vue-html5-editor :content="content" :height="500" @change="getData"></vue-html5-editor>
 </div>
 </template>
 <script>
 export default {
-  // name: "vue-html5-editor",
   data() {
     return {
       content: "vue-html5-editor"
     };
+  },
+  methods: {
+    getData(e) {
+      console.log(e);
+    }
   }
 };
 </script>
