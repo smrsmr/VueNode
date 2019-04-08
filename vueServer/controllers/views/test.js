fbd6b4854a32937afbac8ee46e809046508eb92d
smr <1246054720@qq.com>
2019-04-08 23:10:06 +0800

    优化test接口

--- vueServer/controllers/views/test.js
+++ vueServer/controllers/views/test.js
@@ -1,6 +1,9 @@
 module.exports = function (app, Ms) {
 	const jsonParser = Ms.bodyParser.json();
-	let pageStart = null ,pageEnd = null,current=null,pageSize=null;
+	let pageStart = null, pageEnd = null, current = null, pageSize = null,dataNum=[];
+	Ms.Test.find({}).then((msg) => {
+		dataNum = msg;
+	}).catch((err) => { console.log(err); });
 	app.post('/test', jsonParser, (req, res) => {
 		// console.log(req.body);
 		current = req.body.current;
@@ -10,9 +13,7 @@ module.exports = function (app, Ms) {
 		// console.log(pageStart,pageEnd);
 		Ms.Test.find({}).skip(pageStart).limit(pageSize).exec((err, data) => {
 			if (err) throw err;
-			Ms.Test.find({}).then((msg) => {
-				res.json({status:'ok',data:data,total: msg.length });
-			}).catch((err) => { console.log(err); });
+			res.json({status:'ok',data:data,total: dataNum.length });
 		});
 	})
 }
