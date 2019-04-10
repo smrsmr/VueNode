94c452589a0e778694dfeadecabfefe5c3a5d62b
smr <smr@qq.com>
2019-04-10 17:26:40 +0800

    修复 优化

--- vueServer/controllers/views/test.js
+++ vueServer/controllers/views/test.js
@@ -1,19 +1,16 @@
 module.exports = function (app, Ms) {
 	const jsonParser = Ms.bodyParser.json();
-	let pageStart = null, pageEnd = null, current = null, pageSize = null,dataNum=[];
+	let pageStart = null, current = null, pageSize = null,dataNum=[];
 	Ms.Test.find({}).then((msg) => {
 		dataNum = msg;
 	}).catch((err) => { console.log(err); });
 	app.post('/test', jsonParser, (req, res) => {
-		// console.log(req.body);
 		current = req.body.current;
 		pageSize = req.body.pageSize;
 		pageStart = current * pageSize -pageSize; 
-		pageEnd = current * pageSize;
-		// console.log(pageStart,pageEnd);
 		Ms.Test.find({}).skip(pageStart).limit(pageSize).exec((err, data) => {
-			if (err) throw err;
+			if (err) res.json({status:'error',data:err,total: 0 });
 			res.json({status:'ok',data:data,total: dataNum.length });
 		});
-	})
-}
+	});
+};
