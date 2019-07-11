399f95ca4984def2ba28b9e11a1d172ffc597826
shenmaorui <shenmaorui@paohe.cn>
2019-07-11 18:07:31 +0800

    add

---
+++ tree.js
@@ -0,0 +1,38 @@
+data: [
+	{
+		label: '技术中心',
+		children: [
+			{
+				label: '技术经理',
+			},
+			{
+				label: '前端组',
+				children: [
+					{
+						label: '前端'
+					},
+					{
+						label: '后端'
+					}
+				]
+			}
+		]
+	},
+	{
+		label: '业务部',
+		children: [
+			{
+				label: '销售',
+			},
+			{
+				label: '业务专员',
+			},
+		]
+	},
+	{
+		label: '同级部门1',
+	},
+	{
+		label: '同级部门2',
+	},
+]
