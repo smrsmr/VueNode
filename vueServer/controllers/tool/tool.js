c83a4fdd3819f07e7289cae25c07ebd561415cf4
smr <1246054720@qq.com>
2019-11-09 14:00:56 +0800

    update：更新依赖包到最新版。添加广告报名接口。

--- vueServer/controllers/tool/tool.js
+++ vueServer/controllers/tool/tool.js
@@ -1,53 +1,55 @@
 /**
  * 工具模块
-*/
+ */
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 // let connection = mongoose.createConnection('mongodb://mongodb:123456@localhost/mongodb', { useNewUrlParser: true });
-let connection = mongoose.createConnection('mongodb://localhost/mongodb', { useNewUrlParser: true });
+let connection = mongoose.createConnection('mongodb://localhost/mongodb', {
+	useNewUrlParser: true
+});
 
 //设置变量类型
 /**
  * 经销商数据库
  */
-let schema = new mongoose.Schema(
-	{
-		area: String, 
-		prov: String,
-		city: String,
-		dealer: String,
-		name: String,
-		address: String,
-		phone: String,
-		code: String 
-	}
-);
+let schema = new mongoose.Schema({
+	area: String,
+	prov: String,
+	city: String,
+	dealer: String,
+	name: String,
+	address: String,
+	phone: String,
+	code: String
+});
 //设置变量类型
 /**
  * 用户信息数据库
  */
-let user = new mongoose.Schema(
-	{
-		name: String,
-		pass: String,
-		privilege: Number,
-	}
-);
-let test = new mongoose.Schema(
-	{
-		key: Number,
-		name: String,
-		phone: String,
-		url: String,
-		address: String,
-		date: String
-	}
-);
-let Todo = connection.model('dealers', schema);   //链接经销商表
-let Users = connection.model('users', user);          //链接用户数据表
-let Test = connection.model('tests', test);          //测试数据库
+let user = new mongoose.Schema({
+	name: String,
+	pass: String,
+	privilege: Number,
+});
+let test = new mongoose.Schema({
+	key: Number,
+	name: String,
+	phone: String,
+	url: String,
+	address: String,
+	date: String
+});
+let bm = new mongoose.Schema({
+	account: String,
+	phone: String
+})
+let Bm = connection.model('bms', bm); //链接经销商表
+let Todo = connection.model('dealers', schema); //链接经销商表
+let Users = connection.model('users', user); //链接用户数据表
+let Test = connection.model('tests', test); //测试数据库
 module.exports = {
+	Bm: Bm,
 	Todo: Todo,
 	Users: Users,
 	Test: Test,
--- vueServer/controllers/views/dealers.js
+++ vueServer/controllers/views/dealers.js
@@ -2,16 +2,24 @@
 /**
  * 经销商增删改查模块
  */
-global.userName = ''; global.obj = {};
+global.userName = '';
+global.obj = {};
 let currData = null;
 let currentpage = null;
-let pageStart = 0; let pageEnd = 0;
+let pageStart = 0;
+let pageEnd = 0;
 // let	len = 0;
-let n1 = {}; let n2 = { 'area': userName }; let
+let n1 = {};
+let n2 = {
+	'area': userName
+};
+let
 	sObj = null;
 module.exports = function (app, Ms) {
 	// parse application/x-www-form-urlencoded
-	// const bodyParser = Ms.bodyParser.urlencoded({ extended: false });
+	const bodyParser = Ms.bodyParser.urlencoded({
+		extended: false
+	});
 	// parse application/json
 	const jsonParser = Ms.bodyParser.json();
 	// 默认获取经销商数据接口
@@ -22,44 +30,66 @@ module.exports = function (app, Ms) {
 		pageStart = pageEnd - currData;
 		n2.area = userName;
 		// admin 管理员账号返回全部数据  其他账号就返回相应站点的数据
-		obj = userName !== 'admin' ? { area: userName } : {};
+		obj = userName !== 'admin' ? {
+			area: userName
+		} : {};
 		sObj = userName !== 'admin' ? n2 : n1;
 		// console.log(sObj);
 		// 分页查询获取数据
 		Ms.Todo.find(sObj).skip(pageStart).limit(currData).exec((err, data) => {
 			if (err) throw err;
 			Ms.Todo.find(sObj).then((msg) => {
-				res.json({ data, len: msg.length });
-			}).catch((err) => { console.log(err); });
+				res.json({
+					data,
+					len: msg.length
+				});
+			}).catch((err) => {
+				console.log(err);
+			});
 		});
 	});
 	// 登录接口
 	app.post('/login', jsonParser, (req, res) => {
 		// 每次登录前 恢复默认筛选设置
 		n1 = {};
-		n2 = { area: userName };
+		n2 = {
+			area: userName
+		};
 		const name = req.body.name;
 		const pass = req.body.pass;
-		Ms.Users.find({ name }).exec((err, data) => {
+		Ms.Users.find({
+			name
+		}).exec((err, data) => {
 			if (err) throw err;
 			if (data.length !== 0) {
-				const username = { name: req.body.name }; // 要生成token的主题信息
-				const secretOrPrivateKey = 'asdtpor123e12w456fgh12bnhjk552yu';// 这是加密的key（密钥）
+				const username = {
+					name: req.body.name
+				}; // 要生成token的主题信息
+				const secretOrPrivateKey = 'asdtpor123e12w456fgh12bnhjk552yu'; // 这是加密的key（密钥）
 				// 秘钥 + username + 过期时间生成 Token
 				const token = Ms.jwt.sign(username, secretOrPrivateKey, {
 					expiresIn: 60 * 60, // 过期时间 秒
 				});
 				if (pass !== data[0].pass) {
-					res.json({ status: 2, mess: '密码错误' });
+					res.json({
+						status: 2,
+						mess: '密码错误'
+					});
 					return false;
 				}
 				// userName = name;  //报名成功赋值用户名给全局变量
 				global.userName = name; // 报名成功赋值用户名给全局变量
 				res.json({
-					status: 1, mess: 'ok', token, user_name: req.body.name
+					status: 1,
+					mess: 'ok',
+					token,
+					user_name: req.body.name
 				});
 			} else {
-				res.json({ status: 401, mess: '账户不存在' });
+				res.json({
+					status: 401,
+					mess: '账户不存在'
+				});
 			}
 		});
 	});
@@ -73,9 +103,14 @@ module.exports = function (app, Ms) {
 		const secretOrPrivateKey = 'asdtpor123e12w456fgh12bnhjk552yu'; // 这是加密的key（密钥）
 		Ms.jwt.verify(token, secretOrPrivateKey, (err, data) => {
 			if (err) { //  时间失效的时候/ 伪造的token
-				res.send({ status: 10010 });
+				res.send({
+					status: 10010
+				});
 			} else {
-				res.send({ status: 10000, msg: data });
+				res.send({
+					status: 10000,
+					msg: data
+				});
 			}
 		});
 	});
@@ -83,44 +118,90 @@ module.exports = function (app, Ms) {
 	app.post('/delete', jsonParser, (req, res) => {
 		// console.log(req.body.data._id);
 		// 分页查询获取数据
-		Ms.Todo.find({ _id: req.body.data._id }).deleteMany().then(() => {
+		Ms.Todo.find({
+			_id: req.body.data._id
+		}).deleteMany().then(() => {
 			Ms.Todo.find(sObj).skip(pageStart).limit(currData).exec((err, data) => {
 				if (err) throw err;
 				Ms.Todo.find(sObj).then((msg) => {
-					res.json({ data, len: msg.length });
-				}).catch((err) => { console.log(err); });
+					res.json({
+						data,
+						len: msg.length
+					});
+				}).catch((err) => {
+					console.log(err);
+				});
 			});
-		}).catch((err) => { console.log(err); });
+		}).catch((err) => {
+			console.log(err);
+		});
 	});
 	// 编辑经销商数据接口
 	app.post('/edit', jsonParser, (req, res) => {
 		n2.area = userName;
-		Ms.Todo.updateMany({ _id: req.body.id }, {
-			area: req.body.area, prov: req.body.prov, city: req.body.city, code: req.body.code, phone: req.body.phone, name: req.body.name, address: req.body.address
+		Ms.Todo.updateMany({
+			_id: req.body.id
+		}, {
+			area: req.body.area,
+			prov: req.body.prov,
+			city: req.body.city,
+			code: req.body.code,
+			phone: req.body.phone,
+			name: req.body.name,
+			address: req.body.address
 		}).then((msg) => {
 			Ms.Todo.find(sObj).skip(pageStart).limit(currData).exec((err, data) => {
 				if (err) throw err;
 				Ms.Todo.find(sObj).then((msg) => {
-					res.json({ data, len: msg.length });
-				}).catch((err) => { console.log(err); });
+					res.json({
+						data,
+						len: msg.length
+					});
+				}).catch((err) => {
+					console.log(err);
+				});
 			});
-		}).catch((err) => { console.log(err); });
+		}).catch((err) => {
+			console.log(err);
+		});
 	});
 	// 增加经销商数据接口
 	app.post('/addDealer', jsonParser, (req, res) => {
 		Ms.Todo(req.body).save().then((msg) => {
 			Ms.Todo.find({}, (err, data) => {
 				if (err) throw err;
-				res.json({ status: 1, data: msg });
+				res.json({
+					status: 1,
+					data: msg
+				});
 			});
-		}).catch((err) => { console.log(err); });
+		}).catch((err) => {
+			console.log(err);
+		});
+	});
+	// 广告报名接口
+	app.post('/bmUserInfo', bodyParser, (req, res) => {
+		Ms.Bm(req.body).save().then((msg) => {
+			Ms.Bm.find({}, (err, data) => {
+				if (err) throw err;
+				res.json({
+					status: 1,
+					data: msg
+				});
+			});
+		}).catch((err) => {
+			console.log(err);
+		});
 	});
 	// 获取省份接口
 	app.get('/getArea', (req, res) => {
 		Ms.Todo.find(obj).exec((err, data) => {
 			if (err) {
 				//throw err;
-				res.json({ status: 0, data: err });
+				res.json({
+					status: 0,
+					data: err
+				});
 				return false;
 			}
 			const arrArea = [];
@@ -129,21 +210,32 @@ module.exports = function (app, Ms) {
 					arrArea.push(data[index].prov);
 				}
 			});
-			res.json({ status: 1, data: arrArea });
+			res.json({
+				status: 1,
+				data: arrArea
+			});
 		});
 	});
 	// 查找对应省份经销商
 	app.post('/setProv', jsonParser, (req, res) => {
 		if (req.body.prov !== '') {
-			n1 = { prov: req.body.prov };
-			n2 = { area: userName, prov: req.body.prov };
+			n1 = {
+				prov: req.body.prov
+			};
+			n2 = {
+				area: userName,
+				prov: req.body.prov
+			};
 		} else {
 			n1 = {};
-			n2 = { area: userName };
+			n2 = {
+				area: userName
+			};
 		}
 		sObj = userName !== 'admin' ? n2 : n1;
 		Ms.Todo.find(obj).then((data) => {
-			const arrCity = []; let
+			const arrCity = [];
+			let
 				arrDea = [];
 			data.forEach((value, index) => {
 				if (!arrCity.includes(data[index].city) && req.body.prov === data[index].prov) {
@@ -155,32 +247,63 @@ module.exports = function (app, Ms) {
 				}
 			});
 			res.json({
-				status: 1, data: arrCity, dea: arrDea, len: arrDea.length
+				status: 1,
+				data: arrCity,
+				dea: arrDea,
+				len: arrDea.length
 			});
 		}).catch((err) => {
-			res.json({ status: 0, data: err });
+			res.json({
+				status: 0,
+				data: err
+			});
 		});
 	});
 	// 查找对应城市经销商接口
 	app.post('/setCity', jsonParser, (req, res) => {
 		if (req.body.prov !== '' && req.body.city !== '') {
-			n1 = { prov: req.body.prov, city: req.body.city };
-			n2 = { area: userName, prov: req.body.prov, city: req.body.city };
+			n1 = {
+				prov: req.body.prov,
+				city: req.body.city
+			};
+			n2 = {
+				area: userName,
+				prov: req.body.prov,
+				city: req.body.city
+			};
 		} else if (req.body.prov === '' && req.body.city !== '') {
-			n1 = { city: req.body.city };
-			n2 = { area: userName, city: req.body.city };
+			n1 = {
+				city: req.body.city
+			};
+			n2 = {
+				area: userName,
+				city: req.body.city
+			};
 		} else if (req.body.prov !== '' && req.body.city === '') {
-			n1 = { prov: req.body.prov };
-			n2 = { area: userName, prov: req.body.prov };
+			n1 = {
+				prov: req.body.prov
+			};
+			n2 = {
+				area: userName,
+				prov: req.body.prov
+			};
 		} else if (req.body.prov === '' && req.body.city === '') {
 			n1 = {};
-			n2 = { area: userName };
+			n2 = {
+				area: userName
+			};
 		}
 		sObj = userName !== 'admin' ? n2 : n1;
 		Ms.Todo.find(sObj).then((data) => {
-			res.json({ status: 1, data: data });
+			res.json({
+				status: 1,
+				data: data
+			});
 		}).catch((err) => {
-			res.json({ status: 0, data: err });
+			res.json({
+				status: 0,
+				data: err
+			});
 		});
 	});
 };
